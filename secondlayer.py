498783b7cb69db8054132bafe970b8fe14d5c0c5
Adrien F Vincent <vincent.adrien@gmail.com>
2017-05-21 13:44:01 +0200

    Start accelerating the loop part of Iris_1LayerClassification_Device

--- secondlayer.py
+++ secondlayer.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from numba import jit  # playing with JIT compiler
+from numba import jit, vectorize  # playing with JIT compiler
 
 
 def _gdummyv_vanilla(G, g, m, n, error,memarray):
@@ -320,6 +320,151 @@ def gdummynvana(G, gmax, gmin, g, m, n, error, deltas):
     return _gdummynvana_numpy(G, gmax, gmin, g, m, n, error, deltas)
 
 
+# NB: *vectorize* allows numba to use it durint JIT-compilation
+@vectorize(nopython=True)
+def sign(x):
+    """Apparently np.sign is not supported by Numba yet. So..."""
+    if x > 0:
+        return 1
+    if x < 0:
+        return -1
+    return 0
+
+
+@jit(nopython=True)
+def _compute_error_v_jit(xit, yit, outs):
+    nfts = len(xit)  # convenience variable
+    error = np.zeros((2*nfts + 2, len(yit)))
+
+    for m, (out_val, yit_val) in enumerate(zip(outs, yit)):
+
+        if sign(out_val) != sign(yit_val): # active error-case
+        #if ind == m: # trained neuron
+
+            if sign(out_val) == 1:  # HL case > decr pair weights
+
+                for n in range(nfts):
+                    if sign(xit[n]) == 1:
+                        error[2*n, m] = -1
+                        error[2*n + 1, m] = 1
+                    else:
+                        error[2*n, m] = 1
+                        error[2*n + 1, m] = -1
+
+                error[2*nfts, m] = -1  # originally: error[2*nfts] = -1
+                error[2*nfts + 1, m] = 1  # originally: error[2*nfts+1] = 1
+
+            else: # LH case > incr pair weights
+
+                for n in range(nfts):
+                    if sign(xit[n]) == 1:
+                        error[2*n, m] = 1
+                        error[2*n + 1, m] = -1
+                    else:
+                        error[2*n, m] = -1
+                        error[2*n + 1, m] = 1
+
+                error[2*nfts, m] = 1  # formerly: error[2*nfts] = 1
+                error[2*nfts + 1, m] = -1  # formerly: error[2*nfts+1] = -1
+
+    return error
+
+
+def _compute_error_v_numpy(xit, yit, outs):
+    nfts = len(xit)  # convenience variable
+    error = np.zeros((2*nfts + 2, len(yit)))
+
+    # TODO: one could also try to vectorize this loop...
+    outs_signs = np.sign(outs)  # convenience variable
+    sign_comparisons = np.equal(outs_signs,  np.sign(yit))
+    in_condition = (np.sign(xit) == 1)
+
+    # Misc. convenience variables
+    n_ind = np.arange(nfts, dtype=int)
+    even_ind = 2*n_ind
+    odd_ind = 2*n_ind + 1
+
+    for m, (out_sign, no_error) in enumerate(
+                                        zip(outs_signs, sign_comparisons)):
+
+        if no_error: # no error-case
+            continue
+
+        ref_val = -1 if (out_sign == 1) else 1
+
+        error[even_ind, m] = np.where(in_condition, ref_val, -ref_val)
+        error[odd_ind, m] = np.where(in_condition, -ref_val, ref_val)
+
+        error[2*nfts, m] = ref_val  # formerly: error[2*nfts] = ref_val
+        error[2*nfts + 1, m] = -ref_val  # formerly: error[2*nfts+1] = -ref_val
+
+    return error
+
+
+def _compute_error_v_vanilla(xit, yit, outs):
+    """More or less Chris' original version. """
+    nfts = len(xit)
+    ncls = len(yit)
+    error = np.zeros((2*nfts + 2, ncls))
+
+    for m in range(ncls):
+
+        if np.sign(outs[m]) != np.sign(yit[m]):  # active error-case
+
+            if np.sign(outs[m]) == 1:  # HL case > decr pair weights
+
+                for n in range(nfts):
+                    if np.sign(xit[n]) == 1:
+                        error[2*n, m] = -1
+                        error[2*n + 1,m] = 1
+                    else:
+                        error[2*n, m] = 1
+                        error[2*n + 1, m] = -1
+
+                error[2*nfts, m] = -1  # formerly: error[2*nfts] = -1
+                error[2*nfts + 1, m] = 1  # formerly: error[2*nfts+1] = 1
+
+            else:  # LH case > incr pair weights
+                for n in range(nfts):
+
+                    if np.sign(xit[n]) == 1:
+                        error[2*n, m] = 1
+                        error[2*n + 1, m] = -1
+                    else:
+                        error[2*n, m] = -1
+                        error[2*n + 1, m] = 1
+
+                error[2*nfts, m] = 1  # formerly: error[2*nfts] = 1
+                error[2*nfts + 1, m] = -1  # formerly: error[2*nfts+1] = -1
+
+    return error
+
+
+def compute_error_v(xit, yit, outs):
+    """Compute the error values at the current time, for the scheme that
+    uses the `secondlayer.gdummyv` function.
+
+    Parameters
+    ----------
+    xit : 1D-ndarray
+        The input values.  The vector length is ``(2*nfts + 2)`` with *nfts*
+        the number of features.
+
+    yit : 1D-ndarray
+        The target output values.  The vector length is the number of classes.
+
+    outs : 1D-ndarray
+        The values of the output neuron.  Same length as *yit*.
+
+    Returns
+    -------
+    error : 2D-ndarray
+        The error values.  Shape is ``(2*nfts + 2, len(yit))``.
+    """
+    # TODO: ensure input vectors with ``U = np.asarray(U)``?
+    return _compute_error_v_jit(xit, yit, outs)
+
+
 #TO DO def Greal(...)
 
 
@@ -335,7 +480,8 @@ if __name__ == '__main__':
 
     # ##################
     # Helper function(s)
-    def get_dummy_data(seed=None):
+    def get_gdummy_data(seed=None):
+        """Return dummy arguments for 'gdummy*'-type functions. """
         _prng = np.random.RandomState(seed)  # for reproducibility
         m = 4*10  # number of features
         n = 3*10  # number classes
@@ -350,6 +496,20 @@ if __name__ == '__main__':
 
         return G, gmax, gmin, g, m, n, error, deltas, memarray
 
+    def get_error_data(seed=None):
+        """Return dummy arguments for 'compute_error_v*'-type functions. """
+        _prng = np.random.RandomState(seed)  # for reproducibility
+        nfts, ncls = 4*10, 3*10  # NB: bigger than in the iris case
+        xit = _prng.uniform(low=-1, high=1, size=nfts)
+        yit = np.zeros(ncls) - 1
+        yit[-1] = 1
+        outs = np.copy(yit)
+        outs += np.random.uniform(low=-0.2, high=0.2, size=len(outs))
+        outs[0] *= np.sign(outs[0]*yit[0])  # ensure correct answer
+        outs[1] *= -np.sign(outs[1]*yit[1])  # ensure wrong answer
+
+        return xit, yit, outs
+
     # #################
     # Testing `gdummyv`
     print("Testing 'gdummyv' implementations: ", end="\t")
@@ -357,7 +517,7 @@ if __name__ == '__main__':
 
     results_A = []
     for func in (_gdummyv_vanilla, _gdummyv_numpy, _gdummyv_jit):
-        G_A, _, _, g, m, n, error, _, memarray = get_dummy_data(seed)
+        G_A, _, _, g, m, n, error, _, memarray = get_gdummy_data(seed)
         results_A.append(func(G_A, g, m, n, error, memarray))
 
     np.testing.assert_allclose(results_A[0], results_A[1])
@@ -371,7 +531,7 @@ if __name__ == '__main__':
 
     results_B = []
     for func in (_gdummynv_vanilla, _gdummynv_numpy, _gdummynv_jit):
-        G_B, gmax, gmin, g, m, n, error, _, _ = get_dummy_data(seed)
+        G_B, gmax, gmin, g, m, n, error, _, _ = get_gdummy_data(seed)
         results_B.append(func(G_B, gmax, gmin, g, m, n, error))
 
     np.testing.assert_allclose(results_B[0], results_B[1])
@@ -385,11 +545,26 @@ if __name__ == '__main__':
 
     results_C = []
     for func in (_gdummynvana_vanilla, _gdummynvana_numpy, _gdummynvana_jit):
-        G2, gmax, gmin, g, m, n, error, deltas, _ = get_dummy_data(seed)
+        G2, gmax, gmin, g, m, n, error, deltas, _ = get_gdummy_data(seed)
         results_C.append(func(G2, gmax, gmin, g, m, n, error, deltas))
 
     np.testing.assert_allclose(results_C[0], results_C[1])
     np.testing.assert_allclose(results_C[0], results_C[2])
+    print("OK!")
+
+    # #########################
+    # Testing `compute_error_v`
+    seed = 246
+    print("Testing 'compute_error_v' implementations: ", end="\t")
+    results = []
+    for func in (_compute_error_v_vanilla,
+                 _compute_error_v_numpy,
+                 _compute_error_v_jit):
+        my_args = get_error_data(seed)
+        results.append(func(*my_args))
+
+    np.testing.assert_allclose(results[0], results[1])
+    np.testing.assert_allclose(results[0], results[2])
     print("OK!")
 
     print("\nIf you are reading this, then everything " +
